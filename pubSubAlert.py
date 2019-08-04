130f5c7c32e72a8edb8e5ef438bf04b9d24a0a7b
Connor Christian <connorjchristian@gmail.com>
2019-08-03 23:09:30 -0400

    Add ability to email a notification of results
    Added a loop to attempt to scrape until successful. Sometimes a publix
    popup will appear and crash the scraper; in case of that event, close
    session and try again up to x amount of trys.
    
    You can also text via email but that depends on the .env setting

--- pubSubAlert.py
+++ pubSubAlert.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import datetime
 import os
 import re
 import sys
+import smtplib
 import time
+from email.message import EmailMessage
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from dotenv import load_dotenv, find_dotenv
@@ -12,34 +15,77 @@ from bs4 import BeautifulSoup
 load_dotenv(find_dotenv())
 
 storeID = os.getenv('STOREID')
-
 sub = 'chicken tender sub' if len(sys.argv) == 1 else sys.argv[1] + " sub"
+counter = 5
 
 options = Options()
 options.headless = bool(int(os.getenv("HEADLESS")))
-browser = webdriver.Firefox(options=options)
 
-# navigate to weeklyad
-browser.get('http://weeklyad.publix.com')
+while counter != 0:
+    browser = webdriver.Firefox(options=options)
 
-# specify store
-browser.find_element_by_class_name('selectStoreBtn').click()
-browser.find_element_by_id('pblx-txtLocation').send_keys(storeID)
-browser.find_element_by_id('pblx-btnStoreSearch').click()
-# browser.find_element_by_class_name('js-selectStore').click()
-time.sleep(5)
-browser.find_element_by_xpath('//button[@data-number="'+storeID+'"]').click()
+    try:
+        # navigate to weeklyad
+        browser.get('http://weeklyad.publix.com')
 
-# Navigate add for subs on sale
-browser.find_element_by_class_name('searchInputField').send_keys(sub)
-browser.find_element_by_id('goBtnSearchPosition').click()
+        # specify store
+        browser.find_element_by_class_name('selectStoreBtn').click()
+        browser.find_element_by_id('pblx-txtLocation').send_keys(storeID)
+        browser.find_element_by_id('pblx-btnStoreSearch').click()
+        # browser.find_element_by_class_name('js-selectStore').click()
+        time.sleep(3)
+        browser.find_element_by_xpath('//button[@data-number="'+storeID+'"]').click()
 
-# parse page
-soup = BeautifulSoup(browser.page_source, features="html.parser")
-results = bool(int(re.match(r'(\d+)', soup.select_one('span[class="nonSneakPeekHeader"]').text).group(0)))
-sale = "are" if results else "not"
+        # Navigate add for subs on sale
+        browser.find_element_by_class_name('searchInputField').send_keys(sub)
+        browser.find_element_by_id('goBtnSearchPosition').click()
+
+        # parse page
+        soup = BeautifulSoup(browser.page_source, features="html.parser")
+        results = bool(int(re.match(r'(\d+)', soup.select_one('span[class="nonSneakPeekHeader"]').text).group(0)))
+
+        sale = "are" if results else "not"
+    except:
+        counter -= 1
+        print("Error encountered:", counter, "attempts left...")
+        time.sleep(10)
+    else:
+        break
+    finally:
+        #close browser
+        browser.close()
 
 # message the masses
-print(sub, sale, "on sale!")
+msg = sub + ' ' + sale +  " on sale!"
 
-browser.close()
+# determine the appropriate sale date range: thurs - wed
+thurs = 3
+first = datetime.datetime.today()
+day = first.weekday()
+if day > thurs:
+    first -= datetime.timedelta(days=day-thurs)
+elif day < thurs:
+    first += datetime.timedelta(days=thurs-day)
+
+last = first + datetime.timedelta(days=6)
+msg += " : " + first.strftime('%-m/%-d') + ' - ' + last.strftime('%-m/%-d')
+print(msg)
+
+
+sender = os.getenv('SENDER')
+passwd = os.getenv('PASSWD')
+receiver = os.getenv('RECEIVER')
+
+if sender != '' and receiver != '' and passwd != '':
+    message = EmailMessage()
+
+    message['From'] = sender
+    message['To'] = receiver
+    message['Subject'] = sys.argv[0].split('/')[-1].split('.')[0]
+    message.set_content(msg)
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(sender, passwd)
+    server.send_message(message)
+    server.quit()
